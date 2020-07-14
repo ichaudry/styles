22d8e03746a1396e0dd88f5581dbefd4ad7e23f0
ichaudry <ibraheem27496@gmail.com>
2020-07-13 17:04:52 -0700

    Pushing style

---
+++ application-profile-page.js
@@ -0,0 +1,6 @@
+$( document ).ready(function() {
+    var formContent= document.getElementsByClassName('confiFormsFormContent')[0]
+    var title = document.getElementById("current-status")
+    formContent.insertAdjacentElement('afterbegin',title)
+    title.style = "display: flex"
+});
