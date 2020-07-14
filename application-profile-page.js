53f24798a5c95f15474008c0eec3ba8fc1e0f0ec
ichaudry <ibraheem27496@gmail.com>
2020-07-13 17:07:56 -0700

    Pushing style

--- application-profile-page.js
+++ application-profile-page.js
@@ -3,4 +3,12 @@ $( document ).ready(function() {
     var title = document.getElementById("current-status")
     formContent.insertAdjacentElement('afterbegin',title)
     title.style = "display: flex"
+
+    //Get current status
+	let currentStatus;
+	$.get(`/ajax/confiforms/rest/filter.action?pageId=30541161&f=bLabsRegister&q=id:${formId}`, function(data, status){
+    currentStatus = data.list.entry[0].fields.status
+    let statusDiv = document.getElementById('current-status');
+	statusDiv.insertAdjacentHTML('beforeend', `<div id="current-status-color">${currentStatus}</div>`);
+  });
 });
